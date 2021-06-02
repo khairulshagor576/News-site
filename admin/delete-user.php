57c9e6a10d13dd29d6841bccd38941de26e8e5c6
Mohammad Khairul Islam <khairulsagor567@gmail.com>
2021-06-02 10:32:09 +0600

    here, i completed crud delete part and for this reason, i have created a delete-user.php page.

---
+++ admin/delete-user.php
@@ -0,0 +1,16 @@
+<?php 
+
+include "config.php";
+
+$id=$_GET['id'];
+$sql="DELETE FROM user WHERE user_id={$id}";
+
+if(mysqli_query($conn,$sql))
+{
+  header("Location: {$hostname}/admin/users.php");
+}else{
+  echo "<p style='color:red;'>Con't Delete Data</p>";	
+}
+
+mysqli_close($conn);
+?>
--- admin/users.php
+++ admin/users.php
@@ -43,7 +43,7 @@ $result=mysqli_query($conn,$sql) or die("Query Failed.");
                                 ?>
                           </td>
                               <td class='edit'><a href='update-user.php?id=<?php echo $row["user_id"];?>'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php?id=<?php echo $row["user_id"];?>'>'><i class='fa fa-trash-o'></i></a></td>
+                              <td class='delete'><a href='delete-user.php?id=<?php echo $row["user_id"];?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           <?php } ?>
                       </tbody>
