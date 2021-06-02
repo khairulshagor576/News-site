2e5ef74b827d63b57c035006b63854c839062903
Mohammad Khairul Islam <khairulsagor567@gmail.com>
2021-06-02 23:34:24 +0600

    now completed login part for this project

--- admin/index.php
+++ admin/index.php
@@ -30,6 +30,36 @@
                             <input type="submit" name="login" class="btn btn-primary" value="login" />
                         </form>
                         <!-- /Form  End -->
+                        <?php
+                        include "config.php";
+
+                        if(isset($_POST['login']))
+                        {
+                            $username=mysqli_real_escape_string($conn,$_POST['username']);
+                            $password=mysqli_real_escape_string($conn,md5($_POST['password']));
+                            $sql="SELECT user_id,username,role FROM user WHERE username='{$username}' AND password='{$password}'";
+                            $result=mysqli_query($conn,$sql) or die("Query Failed!!!!");
+
+                            if(mysqli_num_rows($result)>0)
+                            {
+                                while ($row=mysqli_fetch_assoc($result)) 
+                                {
+                                    session_start();
+                                    $_SESSION['username'] = $row['username'];
+                                    $_SESSION['user_id']  = $row['user_id'];
+                                    $_SESSION['user_role']= $row['role'];
+
+                                    header("Location: {$hostname}/admin/post.php");
+                                }
+
+                            }
+                            else
+                            {
+                                echo "<div>User and Password not matched</div>";
+                            }
+                        }
+
+                        ?>
                     </div>
                 </div>
             </div>
